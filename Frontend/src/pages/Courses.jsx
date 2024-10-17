4c8fa7acbf47a826bf7f88f7512f451e69bb4a5f
Mayank <mayank290404gupta@gmail.com>
2024-10-18 00:23:09 +0530

    updated Card

--- Frontend/src/pages/Courses.jsx
+++ Frontend/src/pages/Courses.jsx
@@ -13,22 +13,21 @@ function Courses() {
   return (
     <div className="py-10 px-4 max-w-7xl mx-auto">
       <h1 className="text-4xl font-bold text-center mb-8">Available Courses</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {data.length === 0 ? (
-          <div>Good Things Take Time</div>
-        ) : (
-          data.slice(3,9).map((val) => (
-            <Card 
-              key={val._id}
-              title={val.title} 
-              description={val.description} 
-              imageLink={val.imageLink} 
-              price={val.price} 
-            />
-          ))
-        )}
-        {/* {console.log(data)} */}
-      </div>
+      {data.length === 0 ? (
+        <div className="w-full text-center">Good Things Take Time</div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {data.slice(3,9).map((val) => (
+          <Card 
+            key={val._id}
+            title={val.title} 
+            description={val.description} 
+            imageLink={val.imageLink} 
+            price={val.price} 
+          />
+        ))}
+        </div>
+      )}
     </div>
   );
 }
