1dd489b506bb29d316715b796a7cd0b4df598999
Mayank <mayank290404gupta@gmail.com>
2024-10-22 15:12:38 +0530

    changes made

--- Frontend/src/pages/Admin/AdminCourses.jsx
+++ Frontend/src/pages/Admin/AdminCourses.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Card from "../../components/card";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp,faArrowDown } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowUp,faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function AdminCourses() {
   const [data, setData] = useState([]);
@@ -27,18 +27,12 @@ function AdminCourses() {
     });
   }, [token]);
 
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/admin/courses")
-  //     .then((res) => res.json())
-  //     .then((res) => setData(res.courses));
-  // }, []);
-
   return (
     <div className="py-10 px-4 max-w-7xl mx-auto">
       <h1 className="text-4xl font-bold text-center mb-8">Your Courses</h1>
       <div className="bg-blue-100 p-5 flex gap-5">
         <input type="input" placeholder="Search for Best Course" className="pl-3 py-2 pr-24 rounded-lg"></input>
-        <button>
+        {/* <button>
           <div className="flex items-center gap-1">
             <div>Price</div>
             <FontAwesomeIcon icon={faArrowDown} />
@@ -49,7 +43,7 @@ function AdminCourses() {
             <div>Rating</div>
             <FontAwesomeIcon icon={faArrowDown} />
           </div>
-        </button>
+        </button> */}
       </div>
       {data.length === 0 ? (
         <div className="w-full text-center">Good Things Take Time</div>
