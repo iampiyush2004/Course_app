0b9e94be74126b5eac274c6a499932655886df2b
iampiyush2004 <117506198+iampiyush2004@users.noreply.github.com>
2024-10-27 15:19:44 +0530

    added multer and cloudinary for changing dp and updating backend

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    jwt_secret : "weekndisbest"
+    jwt_secret : "weekndisbest",
+    cloud_name: 'dpzeeteck', 
+    api_key: '367895277947894', 
+    api_secret: 'juice pila do'
 }
