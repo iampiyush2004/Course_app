e71ce23300afcd03b46431c5c224e4a743e05650
iampiyush2004 <117506198+iampiyush2004@users.noreply.github.com>
2024-10-27 11:56:13 +0530

    "changed the schema"

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -23,27 +23,99 @@ const AdminSchema = new mongoose.Schema({
   
 
 const UserSchema = new mongoose.Schema({
-    username : String,
-    password : String,
+    name: String,
+    dob: String,
+    
+    gender : String,
+    institution : String,
+   
+    avatar: String,
+    username: String,
+    password: String,
+    email: String,
+    
     purchasedCourses : [{
         type : mongoose.Schema.Types.ObjectId,
         ref : 'Course'
     }]
-});
+} ,{timestamps : true});
 
 const CourseSchema = new mongoose.Schema({
     title: String,
     description : String,
     imageLink : String, 
-    price : Number
+    price : Number,
+    isCompleted : Boolean,
+    usersEnrolled : Number,
+    category : String,
+    teacher : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Admin'
+    },
+    reviews : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Review'
+    }],
+    videos : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Video'
+    }]
 });
+
+const ReviewSchema = new mongoose.Schema({
+    comment : String,
+    star : Number
+})
+
+
+const videoSchema = new mongoose.Schema({
+    videoFile:{
+      type : String,
+      required : true
+    },
+    thumbnail:{
+      type : String,
+      required : true
+    },
+    owner:{
+      type : mongoose.Schema.Types.ObjectId,
+      ref : "User"
+    },
+    title:{
+      type : String,
+      required : true,
+      index : true
+    },
+    description : {
+      type : String,
+      required : true
+    },
+    duration : {
+      type : Number,
+      required : true
+    },
+    views : {
+      type : Number,
+      default : 0
+    },
+    isPublished : {
+      type : Boolean,
+      default : true
+    }
+  },{timestamps:true})
+  
+
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
+const Review = mongoose.model('Review' , ReviewSchema );
+const Video = mongoose.model('Video' , videoSchema);
 
 module.exports = {
     Admin,
     User,
-    Course
+    Course,
+    Review,
+    Video
 }
