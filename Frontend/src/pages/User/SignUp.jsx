844f46d460d3812a4b194e801cfd27ca2babf5e8
iampiyush2004 <117506198+iampiyush2004@users.noreply.github.com>
2026-02-22 19:20:38 +0530

    bug_fix : optional pic upload and interactive course tiles

--- Frontend/src/pages/User/SignUp.jsx
+++ Frontend/src/pages/User/SignUp.jsx
@@ -126,7 +126,6 @@ const SignUp = ({
           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           id="avatar"
           onChange={(e) => setAvatar(e.target.files[0])}
-          required
         />
       </div>
 
