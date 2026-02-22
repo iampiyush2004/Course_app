fd13062ff1c25abf89636373da2e27a1abdb2506
iampiyush2004 <117506198+iampiyush2004@users.noreply.github.com>
2026-02-23 02:41:45 +0530

    removed nodejs backend folder

--- Backend/db/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = async() => {
-  const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING
-  mongoose.connect(MONGO_CONNECTION_STRING);
-}
--- Backend/routes/progress.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const verifyJwt = require("../middleware/authUser.middleware")
-const {updateProgress, getProgress} = require("../controllers/progress.controller")
-
-router.put("/update/:courseId",verifyJwt,updateProgress)
-router.get("/getProgress/:courseId",verifyJwt,getProgress)
-
-module.exports = router
